fae30ba977f94311c130592ac25e0d2fa6d79c3e
Panos Ioannidis <panos277@hotmail.com>
2019-04-24 10:24:06 +0300

    Add basic servlets

---
+++ src/main/java/com/careeroffice/servlet/AdminServlet.java
@@ -0,0 +1,39 @@
+package com.careeroffice.servlet;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@WebServlet({"/AdminServlet", "/admin"})
+public class AdminServlet {
+    /**
+     * Java related serial version UID.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * An instance of the database connection.
+     */
+    @Resource(name = "jdbc/career_office")
+    private DataSource ds;
+
+    /**
+     * Handles all GET requests.
+     */
+    protected void doGet( HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    /**
+     * Handles all POST requests.
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
