a8349c85190f05719d6b8c17e6ed253c3199e59c
Thanos Paravantis <thanosparavantis@gmail.com>
2019-05-01 22:52:20 +0300

    Add some missing files

---
+++ src/main/java/com/careeroffice/servlet/SuperAdminViewUserServlet.java
@@ -0,0 +1,27 @@
+package com.careeroffice.servlet;
+
+import com.careeroffice.service.UserService;
+import com.careeroffice.service.factory.ServiceEnum;
+import com.careeroffice.service.factory.ServiceFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/view_user"})
+public class SuperAdminViewUserServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("id");
+        UserService userService = (UserService) ServiceFactory.getService(ServiceEnum.UserService);
+
+        request.setAttribute("user", userService.findOne(username));
+
+        request.getRequestDispatcher("WEB-INF/views/super_admin/view_user.jsp").forward(request, response);
+    }
+}
---
+++ web/WEB-INF/views/super_admin/view_user.jsp
@@ -0,0 +1,37 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Super Admin | View User</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h2>View: ${user.fullName}</h2>
+<p><a href="<c:url value="/manage_users"/>"><< Back</a></p>
+<hr>
+<h4>Username</h4>
+<p>${user.username}</p>
+
+<h4>Full Name</h4>
+<p>${user.fullName}</p>
+
+<h4>Phone Number</h4>
+<p>${user.phoneNumber}</p>
+
+<h4>Email Address</h4>
+<p>${user.email}</p>
+
+<h4>Role</h4>
+<p>${user.role.title}</p>
+
+<c:if test="${user.company != null}">
+    <h4>Company</h4>
+    <p>${user.company.title}</p>
+</c:if>
+
+<c:if test="${user.department != null}">
+    <h4>Department</h4>
+    <p>${user.department.title}</p>
+</c:if>
+</body>
+</html>
