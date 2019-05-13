01abd1670ee80e95ffa2d34ca6d480e1c66a72ec
Thanos Paravantis <thanosparavantis@gmail.com>
2019-05-13 21:28:05 +0300

    Setup Classified relationships

--- src/main/java/com/careeroffice/service/ClassifiedService.java
+++ src/main/java/com/careeroffice/service/ClassifiedService.java
@@ -3,12 +3,11 @@ package com.careeroffice.service;
 import com.careeroffice.dao.CategoryDao;
 import com.careeroffice.dao.ClassifiedDao;
 import com.careeroffice.dao.CompanyDao;
+import com.careeroffice.dao.KeywordDao;
 import com.careeroffice.dao.factory.DaoEnum;
 import com.careeroffice.dao.factory.DaoFactory;
-import com.careeroffice.model.Category;
-import com.careeroffice.model.Classified;
-import com.careeroffice.model.Company;
-import com.careeroffice.service.pivot.KeywordClassifiedPivotService;
+import com.careeroffice.dao.pivot.KeywordClassifiedPivotDao;
+import com.careeroffice.model.*;
 
 import java.util.List;
 
@@ -17,26 +16,46 @@ public class ClassifiedService implements IPersistenceService<Classified> {
     private ClassifiedDao classifiedDao = (ClassifiedDao) DaoFactory.getDao(DaoEnum.ClassifiedDao);
     private CompanyDao companyDao = (CompanyDao) DaoFactory.getDao(DaoEnum.CompanyDao);
     private CategoryDao categoryDao = (CategoryDao) DaoFactory.getDao(DaoEnum.CategoryDao);
-    private KeywordClassifiedPivotService keywordClassifiedPivotService = (KeywordClassifiedPivotService) DaoFactory.getDao(DaoEnum.KeywordClassifiedPivotDao);
+    private KeywordDao keywordDao = (KeywordDao) DaoFactory.getDao(DaoEnum.KeywordDao);
+    private KeywordClassifiedPivotDao keywordClassifiedPivotDao = (KeywordClassifiedPivotDao) DaoFactory.getDao(DaoEnum.KeywordClassifiedPivotDao);
 
     public ClassifiedService() {
+
     }
 
     private void setupRelationships(Classified classified) {
-
         Company company = companyDao.findOne(classified.getCompanyId());
         Category category = categoryDao.findOne(classified.getCategoryId());
 
         classified.setCompany(company);
         classified.setCategory(category);
+
+        List<KeywordClassifiedPivot> keywordClassifiedPivotList = keywordClassifiedPivotDao.findByClassified(classified.getId());
+
+        keywordClassifiedPivotList.forEach(keywordClassifiedPivot -> {
+            keywordClassifiedPivot.setClassified(classified);
+
+            Keyword keyword = keywordDao.findOne(keywordClassifiedPivot.getKeywordId());
+            keywordClassifiedPivot.setKeyword(keyword);
+        });
+
+        classified.setKeywordClassifiedPivotList(keywordClassifiedPivotList);
     }
 
     public List<Classified> findAllByCompany(String companyId) {
-        return classifiedDao.findAllByCompany(companyId);
+        List<Classified> classifieds = classifiedDao.findAllByCompany(companyId);
+
+        classifieds.forEach(this::setupRelationships);
+
+        return classifieds;
     }
 
     public List<Classified> findAll() {
-        return classifiedDao.findAll();
+        List<Classified> classifieds = classifiedDao.findAll();
+
+        classifieds.forEach(this::setupRelationships);
+
+        return classifieds;
     }
 
     public String findClassifiedSkills(Integer id, String type) {
